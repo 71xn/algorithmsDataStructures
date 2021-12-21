286df919a19561da683a30c78f4ef219cc6ade59
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2021-12-21 10:05:56 +0000

    Create BreadthFirstSearch.java

---
+++ algorithms/src/tech/finnlestrange/BreadthFirstSearch.java
@@ -0,0 +1,27 @@
+// Breadth First Search in Java 16 - Finn Lestrange 2021
+
+/*
+* Breadth First Search
+* --------------------
+*
+* Searching algo for traversing a tree or graph data structure.
+* This is done by searching one level at a time rather than one branch at a time (like DFS)
+*
+* Here, we use a queue to traverse in order, rather than a stack like in DFS
+*
+* */
+
+// See tech.finnlestrange.graphs.Graph.java for code
+
+
+package tech.finnlestrange;
+
+// Importing graph packages
+import tech.finnlestrange.graphs.Graph;
+import tech.finnlestrange.graphs.Node;
+
+public class BreadthFirstSearch {
+    public static void main(String[] args) {
+
+    }
+}
