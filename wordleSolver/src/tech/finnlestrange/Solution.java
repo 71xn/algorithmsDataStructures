ac803ac34da35f6a653583fb85c855e72a8937b8
Finn Lestrange <44287141+71xn@users.noreply.github.com>
2022-02-16 18:08:23 +0000

    added ranking based on english language frequency

--- wordleSolver/src/tech/finnlestrange/Solution.java
+++ wordleSolver/src/tech/finnlestrange/Solution.java
@@ -8,6 +8,7 @@ import java.util.*;
 public class Solution {
 
     List<String> words;
+    Map<String, Float> frequencies;
 
     private void readWords(String filename) {
 
@@ -22,6 +23,51 @@ public class Solution {
             e.printStackTrace();
         }
     }
+
+    private void readFrequencies(String filename) {
+        frequencies = new HashMap<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                // split line at comma
+                String[] s = line.split("[,]", 0);
+                // each line will have two parts, first is string, second is integer of frequency
+                frequencies.put(s[0], Float.valueOf(s[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // sort
+        Collections.sort(words);
+    }
+
+    // rank words based on their frequencies
+    private List<String> rankWords(List<String> words) {
+        List<String> rankedWords = new LinkedList<>();
+        Map<Float, String> ranked = new TreeMap<>(); // to sort by natural order
+        List<Float> rankingValues = new LinkedList<>();
+        for (String word : words) {
+            rankingValues.add(frequencies.get(word));
+            ranked.put(frequencies.get(word), word);
+        }
+
+        Collections.sort(rankingValues);
+
+        for (Float f : rankingValues) {
+            String s = ranked.get(f);
+            rankedWords.add(s);
+        }
+
+        // best words are at the end, need to reverse
+        Collections.reverse(rankedWords);
+
+        return rankedWords;
+
+    }
+
+
 
     // checks if all chars in string s1 are in string s2
     private boolean checkString(String s1, String s2) {
@@ -52,9 +98,6 @@ public class Solution {
         * knownInPos stores the position of known letters if user chooses to add them
         * */
 
-        readWords("words.txt");
-
-        Collections.sort(words);
 
         // optimizing for words with only known characters
         List<String> potentialWords = new LinkedList<>();
@@ -84,8 +127,6 @@ public class Solution {
             }
         }
 
-
-
         // optimizing for letters in certain positions
         List<String> toRemove = new LinkedList<>();
         for (String word : potentialWords) {
@@ -106,13 +147,15 @@ public class Solution {
         Map<Integer, String> knownInPos = new HashMap<>();
         Map<Integer, String> knownButNotInPos = new HashMap<>();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Wordle Solver: ");
-        System.out.print("\nPlease input the letters you know with no spaces: ");
+        System.out.println("\u001B[106m" + "\u001B[90m" + "Wordle Solver" + "\u001B[0m");
+        System.out.print("\nPlease enter all known letter, regardless of if they are in the correct place, ex. abc : ");
 
         String known = scanner.nextLine();
         known = known.toLowerCase(Locale.ROOT);
 
         // positions of letters in words but in wrong position
+        System.out.println();
+        System.out.println("Please enter the letters that are in the word, but in the wrong position: ");
         for (int i = 0; i < 5; i++) {
             System.out.print("Right letter wrong position (" + (i + 1) + ") : ");
             String line = scanner.nextLine().toLowerCase(Locale.ROOT);
@@ -120,11 +163,12 @@ public class Solution {
             else knownButNotInPos.put(i, line);
         }
 
-        System.out.print("\nPlease input the letters you know are not in the word with no spaces: ");
+        System.out.print("\nPlease input the letters you know are not in the word ex. hyg : ");
 
         String not = scanner.nextLine();
         not = not.toLowerCase(Locale.ROOT);
 
+        System.out.println();
         System.out.println("Please input the letters you know the positions of: ");
 
         for (int i = 0; i < 5; i++) {
@@ -136,13 +180,19 @@ public class Solution {
 
         for (int i = 0; i < knownInPos.size(); i++) if (knownInPos.get(i) == "" || knownInPos.get(i) == null) knownInPos.remove(i);
 
-        System.out.println("\nPotential Solutions: ");
+        System.out.println("\nPotential Solutions (ranked based on frequency in the english language): ");
         List<String> w = optimizeLikelyWord(known, not, knownInPos, knownButNotInPos);
+        w = rankWords(w); // ranks the words based on their
         System.out.println(w);
-
     }
 
     public Solution() {
+        // read data
+        readWords("words.txt");
+        readFrequencies("frequency.txt");
+
+        // System.out.println(frequencies.get("which"));
+
         while (true) {
             output();
             System.out.println();
